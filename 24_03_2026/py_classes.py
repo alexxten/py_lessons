c2ac0664bc3802974c8eae46e65ea0d879b732b0
alexxten <alexxten13@gmail.com>
2026-03-24 22:16:00 +0300

    ls3 classes

---
+++ 24_03_2026/py_classes.py
@@ -0,0 +1,41 @@
+a = 1
+b = "some_string"
+
+def c() -> None:
+    print("Hello World")
+
+
+class Human:
+    def __init__(self, name: str, age: int = 18) -> None:
+        self.name = name
+        self.age = age
+
+    def sleep(self) -> None:
+        print("Sleeping, bye")
+
+    def say_name(self) -> None:
+        print(f"Hello, my name is {self.name}!")
+
+    def say_all_about_me(self) -> None:
+        print(f"{self.name, self.age}")
+
+
+class Zumer(Human):
+    def __init__(self, name: str, elektronka: str, age: int = 18) -> None:
+        super().__init__(name, age)
+        self.elektronka = elektronka
+
+    def my_killer(self) -> None:
+        print(f"I kill myself with {self.elektronka}")
+#
+# human_ann = Human(name="Аня")
+# human_vova = Human(name="Вова", age=25)
+# human_ann.say_name()
+# human_vova.say_name()
+# print(human_vova.name, human_vova.age)
+# human_ann.say_all_about_me()
+
+zumer_olesha = Zumer(name="Olesha", age="19", elektronka="IQOS")
+zumer_olesha.my_killer()
+zumer_olesha.sleep()
+zumer_olesha.say_all_about_me()
